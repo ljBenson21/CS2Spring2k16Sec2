31616048ac11bd7444ce8da2514ff5427c87d93e
ljBenson21 <lbenson18@my.whitworth.edu>
2016-02-23 22:08:33 -0800

    This is an unfinished Quadratic Equations I have some quesitons about it, ask about em on Thursday

---
+++ Quadratic Equation/Quadratic Equation/Source.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+class QuadraticEquation
+{
+private:
+	int a, b, c;
+public:
+	QuadraticEquation()
+		:a(), b(), c()
+	{
+		
+		cout << "Please enter a value for a: ";
+		cin >> a;
+		cout << "Now please enter a value for b: ";
+		cin >> b;
+		cout << "Finally, please enter a value for c: ";
+		cin >> c;
+	}
+
+	int getA() { return a; }
+	int getB() { return b; }
+	int getC() { return c; }
+	double getRoot1()
+{
+	getA(); getB(); getC();
+	double root1 = (-b + sqrt(b * b - (4 * a * c))) / (2 * a);
+	if (root1 < 0)
+		return root1;
+}
+	double getRoot2()
+	{
+		getA(); getB(); getC();
+		double root2 = (-b - sqrt(b * b - (4 * a * c))) / (2 * a);
+		return root2;
+	}
+	double getDisctiminant()
+	{
+	double discriminant = b * b - (4 * a * c);
+	if (discriminant < 0)
+		return 0;
+	else
+		return discriminant;
+	}
+};
+
+
+
+int main(int discriminant)
+{
+
+	QuadraticEquation();
+	
+	if (discriminant > 0)
+		cout << "The roots for the numbers you entered are " << getRoot1() << " and " << getRoot2() << "." << endl;
+	else if (discriminant == 0)
+		cout << "The root for the numbers you entered is " << getRoot1() << endl;
+	else
+		cout << "The numbers you entered have no real roots.";
+
+}
