bcc91f3b387ff082bbc8697456c967b59e89bd9a
ljBenson21 <lbenson18@my.whitworth.edu>
2016-03-15 13:03:33 -0700

    3.1 is now finished but 3.2 is not

--- Homework 3/Exercise 3.1 - Check Palindrome/Main.cpp
+++ Homework 3/Exercise 3.1 - Check Palindrome/Main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
-#include "Word.h"
+//#include "Word.h"
+#include <stack>
+#include <string>
 using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::stack;
+using namespace std;
 
+bool isPalindrome(const string&s);
 
 int main()
 {
-	word fling;
-	fling.changeWord();
-	cout << "Just seeing if this code works.";
+	string s = "";
+	cout << "Please enter a word or phrase. ";
+	getline(cin, s);
+
+	if (isPalindrome(s) == false)
+		cout << s << " is not a palindrome." << endl;
+
+	else if (isPalindrome(s) == true)
+		cout << s << " is a palindrome." << endl;
+
+	
+	cin >> s;
+}
+
+bool isPalindrome(const string& s)
+{
+	if (s.empty())
+		return false;
+	int i = 0;                // first characters
+	int j = s.length() - 1; // last character
+
+	while (i < j)
+	{
+		if (s[i] != s[j])
+		{
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
 }
